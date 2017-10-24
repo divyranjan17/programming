88342c8823e235b63cc81cecae9ce5768df15268
Divy Ranjan <divyranjan90@gmail.com>
2017-10-24 13:29:42 +0000

    Delete code.c

--- hello_world/c/code.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
- //for including function printf() from the header file stdio.h(standard input-output)
-int main()
-{
-	printf("Hello, World! \n"); //'\n' is the newline character to end a line and move the pointer to a new line
-	return 0;
-}
